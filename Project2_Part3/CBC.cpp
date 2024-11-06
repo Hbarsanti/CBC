3fd6a9dd0eb4189bd19882597da0bb42ac10951f
Henry Barsanti <henrybarsanti@Henrys-MacBook-Pro.local>
2024-11-06 15:55:31 -0600

    inital

---
+++ Project2_Part3/CBC.cpp
@@ -0,0 +1,21 @@
+#include "DES.h"
+#include <bitset>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// The blocks has been padded and is already a vector of binary string
+// The key and IV are also binary strings
+
+string CBC_encryption(vector<string> blocks, string key, string IV) {
+  string encrypted;
+  
+  return encrypted;
+}
+
+string CBC_decryption(vector<string> blocks, string key, string IV) {
+  string decrypted;
+  
+  return decrypted;
+}
